ec2d05ed1e02a53ec956f403e39ee1267927266d
Jarjar4779 <jaredcbooker@icloud.com>
2025-01-21 12:28:50 -0500

    fixing stuff

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const PrayerForm = () => {
   useEffect(() => {
     const fetchPrayers = async () => {
       try {
-        const response = await axios.get('https://prayer-site.onrender.com');
+        const response = await axios.get('https://prayer-site.onrender.com/api/prayers');
         setPrayers(response.data);
       } catch (error) {
         console.error('Error fetching prayers:', error);
@@ -26,7 +26,7 @@ const PrayerForm = () => {
     e.preventDefault();
 
     try {
-      const response = await axios.post('https://prayer-site.onrender.com', {
+      const response = await axios.post('https://prayer-site.onrender.com/api/prayers', {
         name,
         prayer,
       });
